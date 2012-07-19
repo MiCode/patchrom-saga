8425d23599f12176e7cfac3eeced4ef5c50073ab
yanhao <yanhao@xiaomi.com>
2012-07-19 15:35:28 +0800

    fix ota bug , mount /sytem/lib at the begining of OTA
    Change-Id: I726b1f0d009e42453cb313850b64b635c5d2749b

--- releasetools.py
+++ releasetools.py
@@ -4,20 +4,27 @@ import edify_generator
 def DeleteFormat(info):
     edify = info.script
     for i in xrange(len(edify.script)):
-        if "unmount(\"/system\")" in edify.script[i]:
-            edify.script[i] = edify.script[i].replace("unmount", 'ui_print')
-        elif "format" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
-            edify.script[i] = edify.script[i].replace("format(\"ext4\", \"EMMC\", \"/dev/block/mmcblk0p25\", \"0\");", 'mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");')
+        if "format" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
+            edify.script[i]="""ui_print("do not formate system");"""
         elif "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
-            edify.script[i]='delete_recursive("/system/app", "/system/bin", "/system/customize", "/system/etc", "/system/fonts", "/system/framework", "/system/lib", "/system/media", "/system/tts", "/system/usr", "/system/xbin","/system/build.prop", "0");'
+            edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");
+delete_recursive("/system/app", "/system/bin", "/system/customize", "/system/etc", "/system/fonts", "/system/framework", "/system/lib", "/system/media", "/system/tts", "/system/usr", "/system/xbin","/system/build.prop", "0");
+mount("ext4", "EMMC", "/dev/block/mmcblk0p28", "/system/lib");"""
     return
+
 def AddAssertions(info):
     info.script.AppendExtra('set_perm_recursive(0, 2000, 06755, 06755, "/system/xbin");');
-    return 
+    info.script.AppendExtra('unmount("/system/lib");')
+    return
 
 def FullOTA_InstallEnd(info):
-    AddAssertions(info)
     DeleteFormat(info)
+    AddAssertions(info)
 
 def IncrementalOTA_InstallEnd(info):
+    edify = info.script
+    for i in xrange(len(edify.script)):
+        if "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
+            edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");
+mount("ext4", "EMMC", "/dev/block/mmcblk0p28", "/system/lib");"""
     AddAssertions(info)
