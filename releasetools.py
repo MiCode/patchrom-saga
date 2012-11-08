a2dc600d31d2aa2ceac3af0fb860f8265b5c644c
yanhao <yanhao@xiaomi.com>
2012-11-08 14:05:36 +0800

    add pre-library which miui build, copy all because there was no libs in previous incrematll OTA on this device
    Change-Id: Id2dcff40df90ab55e78317dc5cccfbe298a74d60

--- releasetools.py
+++ releasetools.py
@@ -25,6 +25,17 @@ def FullOTA_InstallEnd(info):
     DeleteFormat(info)
     AddAssertions(info)
 
+def AddIncrementalLibrary(info):
+    info.output_zip.writestr("lib/libshell_jni.so", info.target_zip.read("SYSTEM/lib/libshell_jni.so"))
+    info.output_zip.writestr("lib/libshellservice.so", info.target_zip.read("SYSTEM/lib/libshellservice.so"))
+    info.output_zip.writestr("lib/libshell.so", info.target_zip.read("SYSTEM/lib/libshell.so"))
+    info.output_zip.writestr("lib/libaudiofp.so", info.target_zip.read("SYSTEM/lib/libaudiofp.so"))
+    info.output_zip.writestr("lib/libjni_resource_drm.so", info.target_zip.read("SYSTEM/lib/libjni_resource_drm.so"))
+    info.output_zip.writestr("lib/libffmpeg_xm.so", info.target_zip.read("SYSTEM/lib/libffmpeg_xm.so"))
+    info.output_zip.writestr("lib/liblocSDK_2.5OEM.so", info.target_zip.read("SYSTEM/lib/liblocSDK_2.5OEM.so"))
+    info.output_zip.writestr("lib/libjni_latinime.so", info.target_zip.read("SYSTEM/lib/libjni_latinime.so"))
+    info.script.AppendExtra('package_extract_dir("lib","/system/lib");');
+
 def IncrementalOTA_InstallEnd(info):
     info.output_zip.writestr("mount_lib.sh", info.target_zip.read("SYSTEM/bin/mount_lib.sh"))
     edify = info.script
@@ -34,4 +45,5 @@ def IncrementalOTA_InstallEnd(info):
 package_extract_file("mount_lib.sh", "/mount_lib.sh");
 run_program("/sbin/busybox", "chmod", "777", /mount_lib.sh);
 run_program("/mount_lib.sh");"""
+    AddIncrementalLibrary(info)
     AddAssertions(info)
